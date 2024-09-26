65609c1e7a35a718b7aadd2363aa912708af2dd9
Ayushman Dash <72654041+SonuDash@users.noreply.github.com>
2024-09-26 18:13:48 +0530

    Update HeroTexts.tsx

--- src/components/HeroTexts.tsx
+++ src/components/HeroTexts.tsx
@@ -6,7 +6,7 @@ const HeroTexts = () => {
   
   return (
     <>
-      <h3 className="font-poppins text-2xl max-sm:text-xl" >My Name is</h3>
+      <h3 className="font-poppins text-2xl max-sm:text-xl" > &nbsp;Hey! I am </h3>
       <h1 className="font-rubik text-8xl name_underline text-primary max-sm:text-6xl " >
         Ayushman <br /> Dash .
       </h1>
