c8bd09792a18c5b978a677ed4e22148003f7f4c7
Ayushman Dash <72654041+SonuDash@users.noreply.github.com>
2024-09-26 18:10:59 +0530

    Update Aboutfooter.tsx

--- src/components/Aboutfooter.tsx
+++ src/components/Aboutfooter.tsx
@@ -1,12 +1,12 @@
-import {Circle, Dna, Globe2, Languages} from "lucide-react"
+import {Circle, Dna, Globe2, Globe2Icon, Languages} from "lucide-react"
 
 const Aboutfooter = () => {
 
 
     const items = [
-        {name:"Language", answer:"English", icon:<Languages className="h-11 w-11" />},
-        {name:"Nationality",answer:"India", icon:<Globe2   className="h-8 w-8" />},
-        {name:"Gender",answer:"Male", icon:<Dna  className="h-8 w-8" />},
+        {name:"Language", answer:"English, Hindi, Odia", icon:<Languages className="h-8 w-8" />},
+        {name:"Nationality",answer:"Indian", icon:<Globe2Icon   className="h-8 w-8" />},
+        {name:"Gender",answer:"Male (He/Him)", icon:<Dna  className="h-8 w-8" />},
     ]
 
 
@@ -18,7 +18,7 @@ const Aboutfooter = () => {
                 <div className="p-1 w-fit relative" key={indx}>
                 <h1 className="gap-2 text-3xl font-poppins text-primary font-semibold relative flex icon_underline max-sm:text-2xl">{val.icon}{val.name}</h1>
                 <div className="flex gap-2 justify-center items-center flex-row text-xl text-primary pt-3 max-lg:justify-start">
-                <Circle className="h-3 w-3" /> {val.answer}
+                <Circle className="h-2 w-2" /> {val.answer}
                 </div>
             </div>
               )
