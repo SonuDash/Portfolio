510d6aba3619a1f1af5a4d50511713e572c4af45
Ayushman Dash <72654041+SonuDash@users.noreply.github.com>
2024-09-26 18:07:34 +0530

    Update page.tsx

--- src/app/education/page.tsx
+++ src/app/education/page.tsx
@@ -7,6 +7,7 @@ const educationPage = () => {
   return (
     // ABOUT PAGE
     <div className="h-full w-full relative flex flex-col items-start gap-5 overflow-hidden">
+      <br />
       <Badge className="gap-2">
         <Briefcase className="h-5 w-5"/>
         Work
@@ -67,7 +68,7 @@ const educationPage = () => {
       <div className="w-full h-fit flex flex-col">
         <div className="w-full h-fit flex">
           <FramerWrapper y={0} x={-100} delay={0.35} className="w-1/4 font-rubik flex items-center justify-evenly text-lg max-sm:text-base ">
-            Aug (2020 - 2024)
+            2020 - 2024
           </FramerWrapper>
           <FramerWrapper y={0} x={100} delay={0.35} className="relative w-3/4 border-l-4 border-l-[#3c3c3c] p-4 gap-3 education_point ">
             <div className="text-2xl font-rubik max-sm:text-xl">
